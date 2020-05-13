6b322d77184c6cf7e4927dbefe6f9792baf1697f
Kermina Awad <kerminaawad@gmail.com>
2020-05-13 17:23:50 -0400

    add a test for the migrate command

--- src/test/java/com/onarandombox/multiverseinventories/TestCommands.java
+++ src/test/java/com/onarandombox/multiverseinventories/TestCommands.java
@@ -11,9 +11,13 @@ import com.onarandombox.MultiverseCore.MultiverseCore;
 import com.onarandombox.multiverseinventories.share.Sharables;
 import com.onarandombox.multiverseinventories.util.TestInstanceCreator;
 import junit.framework.Assert;
+import org.bukkit.Material;
 import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPluginLoader;
@@ -26,6 +30,8 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -46,6 +52,24 @@ public class TestCommands {
         assertTrue(creator.setUp());
         mockServer = creator.getServer();
         mockCommandSender = creator.getCommandSender();
+    }
+
+    public void addToInventory(PlayerInventory inventory, Map<Integer, ItemStack> items) {
+        for (Map.Entry<Integer, ItemStack> invEntry : items.entrySet()) {
+            inventory.setItem(invEntry.getKey(), invEntry.getValue());
+        }
+    }
+
+    public static Map<Integer, ItemStack> getFillerInv() {
+        Map<Integer, ItemStack> fillerItems = new HashMap<Integer, ItemStack>();
+        fillerItems.put(3, new ItemStack(Material.BOW, 1));
+        fillerItems.put(13, new ItemStack(Material.DIRT, 64));
+        fillerItems.put(36, new ItemStack(Material.IRON_HELMET, 1));
+        ItemStack book = new ItemStack(Material.WRITTEN_BOOK, 1);
+        fillerItems.put(1, book);
+        ItemStack leather = new ItemStack(Material.LEATHER_BOOTS, 1);
+        fillerItems.put(2, leather);
+        return fillerItems;
     }
 
     @After
@@ -170,5 +194,38 @@ public class TestCommands {
 
         cmdArgs = new String[]{ "info", "default" };
         plugin.onCommand(mockCommandSender, mockCommand, "", cmdArgs);
+    }
+
+    @Test
+    public void testMigrateCommand() {
+        // Pull a core instance from the server.
+        Plugin plugin = mockServer.getPluginManager().getPlugin("Multiverse-Inventories");
+        MultiverseInventories inventories = (MultiverseInventories) plugin;
+
+        // Make sure Core is not null
+        assertNotNull(plugin);
+
+        // Make sure Core is enabled
+        assertTrue(plugin.isEnabled());
+
+        // Initialize a fake command
+        Command mockCommand = mock(Command.class);
+        when(mockCommand.getName()).thenReturn("mvinv");
+
+        Command mockCoreCommand = mock(Command.class);
+        when(mockCoreCommand.getName()).thenReturn("mv");
+
+        Player player = inventories.getServer().getPlayer("dumptruckman");
+        Player player2 = inventories.getServer().getPlayer("dumptruckman2");
+
+        addToInventory(player.getInventory(), getFillerInv());
+        String originalInventory = player.getInventory().toString();
+        String emptyInventory = player2.getInventory().toString();
+
+        String[] cmdArgs = new String[]{"migrate", "dumptruckman", "dumptruckman2"};
+        inventories.onCommand(mockCommandSender, mockCommand, "", cmdArgs);
+
+        Assert.assertEquals(emptyInventory, player.getInventory().toString());
+        Assert.assertEquals(originalInventory, player2.getInventory().toString());
     }
 }
