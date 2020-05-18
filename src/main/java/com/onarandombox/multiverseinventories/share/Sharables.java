f3f070f5ec6de31e0416c148bc0f4177a47917f7
Kermina Awad <kerminaawad@gmail.com>
2020-05-17 21:01:17 -0400

    add recipes sharable

--- src/main/java/com/onarandombox/multiverseinventories/share/Sharables.java
+++ src/main/java/com/onarandombox/multiverseinventories/share/Sharables.java
@@ -8,12 +8,15 @@ import com.onarandombox.multiverseinventories.PlayerStats;
 import com.onarandombox.multiverseinventories.profile.PlayerProfile;
 import com.onarandombox.multiverseinventories.util.MinecraftTools;
 import org.bukkit.GameRule;
+import org.bukkit.Keyed;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.Statistic;
 import org.bukkit.advancement.Advancement;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
 import org.bukkit.potion.PotionEffect;
 
 import java.util.Arrays;
@@ -26,6 +29,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * The Sharables class is where all the default Sharable instances are located as constants as well as a factory class
@@ -499,7 +503,8 @@ public final class Sharables implements Shares {
             new SharableHandler<Location>() {
                 @Override
                 public void updateProfile(PlayerProfile profile, Player player) {
-                    //profile.set(LAST_LOCATION, player.getLocation());
+                    // we can't save last_location here! it's too late!
+                    // instead, it's saved in the PlayerTeleportEvent listener.
                 }
 
                 @Override
@@ -661,6 +666,48 @@ public final class Sharables implements Shares {
             }).defaultSerializer(new ProfileEntry(false, "game_statistics")).altName("game_stats").build();
 
     /**
+     * Sharing Recipes.
+     */
+    public static final Sharable<Map> RECIPES = new Sharable.Builder<Map>("recipes", Map.class,
+            new SharableHandler<Map>() {
+                @Override
+                public void updateProfile(PlayerProfile profile, Player player) {
+                    HashSet<NamespacedKey> recipes = new HashSet<>();
+                    Iterator<Recipe> recipeIterator = inventories.getServer().recipeIterator();
+
+                    while (recipeIterator.hasNext()) {
+                        Recipe recipe = recipeIterator.next();
+                        if (recipe instanceof Keyed) {
+                            NamespacedKey key = ((Keyed) recipe).getKey();
+                            if (player.undiscoverRecipe(key)) recipes.add(key);
+                        }
+                    }
+
+                    player.discoverRecipes(recipes);
+                    profile.set(RECIPES, recipes.stream().collect(Collectors.groupingBy(NamespacedKey::getNamespace, Collectors.mapping(NamespacedKey::getKey, Collectors.toList()))));
+                }
+
+                @Override
+                public boolean updatePlayer(Player player, PlayerProfile profile) {
+                    Map<String, List<String>> recipes = profile.get(RECIPES);
+                    recipes = (recipes != null) ? recipes : new HashMap<>();
+                    Iterator<Recipe> recipeIterator = inventories.getServer().recipeIterator();
+
+                    while (recipeIterator.hasNext()) {
+                        Recipe recipe = recipeIterator.next();
+                        if (recipe instanceof Keyed) {
+                            NamespacedKey key = ((Keyed) recipe).getKey();
+                            if (recipes.containsKey(key.getNamespace()) && recipes.get(key.getNamespace()).contains(key.getKey())) {
+                                player.discoverRecipe(key);
+                            } else player.undiscoverRecipe(key);
+                        }
+                    }
+
+                    return !recipes.isEmpty();
+                }
+            }).defaultSerializer(new ProfileEntry(false, "recipes")).build();
+
+    /**
      * Grouping for inventory sharables.
      */
     public static final SharableGroup ALL_INVENTORY = new SharableGroup("inventory",
@@ -704,7 +751,7 @@ public final class Sharables implements Shares {
     public static final SharableGroup ALL_DEFAULT = new SharableGroup("all", fromSharables(HEALTH, ECONOMY,
             FOOD_LEVEL, SATURATION, EXHAUSTION, EXPERIENCE, TOTAL_EXPERIENCE, LEVEL, INVENTORY, ARMOR, BED_SPAWN,
             MAXIMUM_AIR, REMAINING_AIR, FALL_DISTANCE, FIRE_TICKS, POTIONS, LAST_LOCATION, ENDER_CHEST, OFF_HAND,
-            ADVANCEMENTS, GAME_STATISTICS),
+            ADVANCEMENTS, GAME_STATISTICS, RECIPES),
             "*", "everything");
 
 
